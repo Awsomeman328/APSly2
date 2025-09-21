061eac8f8b052c10ed604363ca50697966733478
Awsomeman328 <Awsoemman328@gmail.com>
2025-09-20 22:13:46 -0400

    Designing the logical Layout of the user defined shop groups feature

--- Rules.py
+++ Rules.py
@@ -99,6 +99,23 @@ def set_rules(world: "Sly2World"):
                 )
             )
 
+    # TODO: Refactor this (and anywhere else in the code where this logic needs to be 
+    # changed) to use the new user defined variables from the yaml file as follows.
+    # 
+    # thiefnet_groups_size will determine the size of each group (default 3) while 
+    # thiefnet_episode_items_required will determine how many additional progressive 
+    # episode items are required for each group (default 4, though the original of 2 can 
+    # still be fine). If the total number of episodes required goes above 27, then clamp 
+    # the episode requirement to 27 for that group and each group afterward.
+    # 
+    # (Got 27 by [6Eps*4Days + 3Days_for_Ep4] and excluding Ep8's Days as depending on user 
+    # settings they can be excluded from being generated.)
+    # 
+    # Also change the starting group to require either 0 or 1 episode items to be in logic. 
+    # (Either way, check the yaml file to ensure the mention of this part of the logic is
+    # either accurate, or remove it if it is decided that we won't do this change. Or it's 
+    # also possible to make this a user controlled variable too.)
+
     # Putting ThiefNet stuff out of logic, to make early game less slow.
     # Divides the items into 8 groups of 3. First groups requires 2 episodes
     # items to be in logic, second group requires 4, etc.
